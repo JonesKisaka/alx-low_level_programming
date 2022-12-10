d328323e0b6f32d78644023e02de5d37432414fb
root <root@DESKTOP-V64HJES.localdomain>
2022-12-10 04:35:56 +0300

    Variables and loops

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+able File  27 lines (21 sloc)  345 Bytes
+
+  #include <stdio.h>
+
+/**
+ *main - prints all the numbers of base 16 in lowercase, followed by a new line
+ *
+ *Return: always 0
+ */
+
+  int main(void)
+{
+  int  hexan;
+
+  char hexa;
+
+  for (hexan = 0; hexan < 10; hexan++)
+    {
+      putchar((hexan % 10) + '0');
+    }
+  for (hexa = 'a'; hexa <= 'f'; hexa++)
+    {
+      putchar(hexa);
+    }
+
+  putchar('\n');
+
+  return (0);
+}

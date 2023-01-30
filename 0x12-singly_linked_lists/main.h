febf4b94ce5d69e06ca929f99712cc2af64bb9e7
root <root@KISAKATHEJONES>
2023-01-30 22:52:57 +0300

    README

---
+++ 0x12-singly_linked_lists/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void size_t print_list(const list_t *h)
+	void size_t list_len(const list_t *h)
+	void list_t *add_node(list_t **head, const char *str)
+	void list_t *add_node_end(list_t **head, const char *str)
+	void free_list(list_t *head)
+#endif //MAIN_H
